2abcde1edd9894857529fdfe3a78bd0b819dcd61
taofeeq billyhadiat <billyhadiattaofeeq@gmail.com>
2025-07-05 11:34:07 +0100

    solve little issue migration

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('notification');
+    return view('welcome');
 });
 
 Route::post('/migrate', function () {
