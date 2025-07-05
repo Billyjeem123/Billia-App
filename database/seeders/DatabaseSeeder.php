4ca5329ae098f58ad4266cfde364aa89d462eca8
taofeeq billyhadiat <billyhadiattaofeeq@gmail.com>
2025-07-05 12:21:19 +0100

    added seeder to databs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,13 +10,15 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    // database/seeders/DatabaseSeeder.php
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            TierSeeder::class,
+            RoleSeeder::class
+            // Add other seeders here
+        ]);
     }
+
 }
